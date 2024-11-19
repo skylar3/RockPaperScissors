16dde8a6abf59f5d44bc6c422445f53c72458e37
skylar3 <skylartipton01@yahoo.com>
2024-11-19 14:02:54 -0500

    added user input a random computer choice and logic to play the game

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-console.log("hello");
+function getComputerChoice()
+{
+    let rand = Math.floor(Math.random() * 2);
+    if (rand == 0)
+    {
+        let choice = "rock";
+        return choice;
+    }
+    else if (rand == 1)
+    {
+        let choice = "paper";
+        return choice;
+    }
+    else if (rand == 2)
+    {
+        let choice = "scissors";
+        return choice;
+    }
+}
+function getHumanChoice()
+{
+    let choice = prompt("Choose Rock Paper or Scissors");
+    choice.toLowerCase;
+    return choice;
+
+}
+
+function playRound(humanChoice, computerChoice)
+{
+    if (humanChoice === computerChoice)
+    {
+        console.log("Tie!")
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper")
+    {
+        console.log("the computer wins paper beats rock")
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock")
+    {
+            console.log("the human wins paper beats rock")
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper")
+        {
+            console.log("the human wins scissors beats paper")
+        }
+        else if (humanChoice == "paper" && computerChoice == "scissors")
+            {
+                console.log("the computer wins scissors beats paper")
+            }
+}
+console.log(playRound(getHumanChoice(), getComputerChoice()));
